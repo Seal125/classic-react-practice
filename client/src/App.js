13116807d776c8f5d7bbc7f1fa5401462ebb2cc6
Stephanie Smith <stephaniesmith12514@gmail.com>
2023-03-30 20:21:10 -0400

    adding components and form, attempting to understand props

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <h1>Hello there!</h1>
-        <h2>
-          Here's a form that you can fill out, it won't do anything though
-        </h2>
+        <main className='App'>
+          <h1>Hello there!</h1>
+          <h2>
+            Here's a form that you can fill out, it won't do anything though 🙂
+          </h2>
+
+          <Form />
+        </main>
       </>
     );
   }
